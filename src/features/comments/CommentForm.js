5fe9cac24a254132d7305e3c35c37348f1e7f98f
Jiyun Lee <kuri12071112@gmail.com>
2024-10-30 15:04:42 +0700

    comments part added

--- src/features/comments/CommentForm.js
+++ src/features/comments/CommentForm.js
@@ -3,7 +3,8 @@ import { useDispatch } from 'react-redux';
 import { Button, Modal, ModalHeader, ModalBody, Label, FormGroup } from 'reactstrap';
 import { Formik, Field, Form, ErrorMessage } from 'formik';
 import { validateCommentForm } from '../../utils/validateCommentForm';
-import { addComment } from './commentsSlice';
+import { postComment } from './commentsSlice';
+
 
 const CommentForm = ({ campsiteId }) => {
 
@@ -19,7 +20,7 @@ const CommentForm = ({ campsiteId }) => {
             date: new Date(Date.now()).toISOString()
         }
         console.log('comment:', comment);
-        dispatch(addComment(comment));
+        dispatch(postComment(comment));
         setModalOpen(false);
     }
 
--- src/features/comments/CommentsList.js
+++ src/features/comments/CommentsList.js
@@ -3,23 +3,35 @@ import { Col } from 'reactstrap';
 import Comment from './Comment';
 import { selectCommentsByCampsiteId } from './commentsSlice';
 import CommentForm from './CommentForm';
+import Error from '../../components/Error';
+import Loading from '../../components/Loading';
 
 const CommentsList = ({ campsiteId }) => {
     const comments = useSelector(selectCommentsByCampsiteId(campsiteId));
+    const isLoading = useSelector((state) => state.campsites.isLoading);
+    const errMsg = useSelector((state) => state.campsites.errMsg);
+
+    if (isLoading) {
+        return <Loading />
+    }
+
+    if (errMsg) {
+        return <Error errMsg={errMsg} />
+    }
 
     if (comments && comments.length > 0) {
         return (
-            <Col md = '5' className = 'm-1' >
+            <Col md='5' className='m-1' >
                 <h4>Comments</h4>
                 {comments.map((comment) => {
                     return <Comment key={comment.id} comment={comment} />;
-                })} 
+                })}
                 <CommentForm campsiteId={campsiteId} />
-                </Col >
+            </Col >
         );
     }
     return (
-        <Col md = '5' className = 'm-1'>
+        <Col md='5' className='m-1'>
             There are no comments for this campsite yet.
         </Col>
     )
